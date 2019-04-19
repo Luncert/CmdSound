aebda98d7b0a1cf980b09dbb26a3e6247d8cd098
Luncert <2725115515@qq.com>
2019-04-20 00:03:38 +0800

    Delete t.py

--- t.py
+++
@@ -1,6 +0,0 @@
-def getCols():
-    import subprocess
-    return int(subprocess.Popen(\
-            ['tput','cols'], stdout=subprocess.PIPE, stderr=subprocess.PIPE\
-        ).communicate()[0])
-print(getCols())
